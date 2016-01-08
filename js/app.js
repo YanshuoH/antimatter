b99ca3b324f64ed2fb803953fbc1de93d7fae8ae
YanshuoH <hys235606751@gmail.com>
2016-01-08 09:49:50 +0100

    init project with particl.js

---
+++ js/app.js
@@ -0,0 +1,99 @@
+var particlesConfig = {
+    particles: {
+        number: {
+            value: 60,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            'value': '#ffffff'
+        },
+        shape: {
+            type: 'circle',
+            stroke: {
+                width: 0,
+                color: '#000000'
+            },
+            polygon: {
+                'nb_sides': 5
+            },
+            image: {
+                src: 'img/github.svg',
+                width: 100,
+                height: 100
+            }
+        },
+        opacity: {
+            value: 1,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.75,
+                sync: false
+            }
+        },
+        size: {
+            value: 5,
+            random: true,
+            anim: {
+                enable: false,
+                speed: 40,
+                size_min: 2,
+                sync: false
+            }
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: '#ffffff',
+            opacity: 0.8,
+            width: 3,
+            grab_color: '#ffff33'
+        },
+        move: {
+            enable: true,
+            speed: 6,
+            direction: 'none',
+            random: false,
+            straight: false,
+            out_mode: 'out',
+            attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+            }
+        }
+    },
+    interactivity: {
+        detect_on: 'canvas',
+        events: {
+            onhover: {
+                enable: true,
+                mode: 'repulse'
+            },
+            resize: true
+        },
+        modes: {
+            grab: {
+                distance: 100,
+                line_linked: {
+                    opacity: 1
+                }
+            },
+            repulse: {
+                distance: 500
+            }
+        }
+    },
+    'retina_detect': true
+};
+
+var init = function() {
+    particlesJS('particles-js', particlesConfig);
+};
+
+
+init();
